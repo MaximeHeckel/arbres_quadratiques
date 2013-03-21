2ca229d9bb60e27b89318088728f1d25b68e3cdf
Phil <philigaultier@gmail.com>
2013-03-21 20:13:05 +0100

    Ajout d'un main + quelques tests (Image marche)

---
+++ ipa/main.c
@@ -0,0 +1,44 @@
+/*
+ * @File : test.c
+ *
+ * @Author :
+ *
+ * @Summary :
+ *
+ * @Version : 2013/03/14
+ *
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ *  wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return
+ * ----------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "arbre.h"
+#include "algos.h"
+#include "image.h"
+
+int main(int argc, char * argv[])
+{
+    /*
+    //Arbre mon_fils = creer(Feuille);
+    Arbre pere = inserer(NULL,SO,NOIR);
+  //  print(pere);
+    Arbre fils = inserer(pere,SE,BLANC);
+    print(pere);
+    //free(pere);
+    printf("\n%d",nb_feuille(pere));
+
+    free_arbre(pere);*/
+    Image image = creerImage(4,4);
+    colorier(image,0,2,2);
+    print_image(image);
+
+
+
+	return 0;
+}
+
